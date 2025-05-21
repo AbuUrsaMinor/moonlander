224edd6c1b8af406f17ec2f5fd85e4b4fe2d6d90
Erik Johansson <erik.b.johansson@afconsult.com>
2025-05-21 17:21:08 +0200

    Initial commit of MoonyLander project

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/moonylander/', // Set base for GitHub Pages deployment
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['vite.svg'],
+      manifest: {
+        name: 'MoonyLander',
+        short_name: 'MoonyLander',
+        description: 'MoonyLander Game',
+        theme_color: '#242424',
+        background_color: '#242424',
+        display: 'standalone',
+        start_url: '/moonylander/',
+        icons: [
+          {
+            src: 'vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+          },
+        ],
+      },
+    }),
+  ],
+})
